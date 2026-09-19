ac16fdde39ec182494c7d3632ca365a7742aa3ba
dhilaryw-19 <dhilaryw@gmail.com>
2026-09-18 22:32:35 -0600

    Hice controladorBatalla

---
+++ src/control/ControladorBatalla.java
@@ -0,0 +1,70 @@
+package control;
+
+import game.CampoBatalla;
+import game.Equipo;
+import modelo.Mutante;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ControladorBatalla{
+    private CampoBatalla campoBatalla;
+    private AdministradorCombate administradorCombate;
+    private List<HiloMutante> hiloMutante;
+
+    //Constructor
+    public ControladorBatalla(CampoBatalla pCampoBatalla, 
+        AdministradorCombate pAdministradorCombate){
+            this.campoBatalla = pCampoBatalla;
+            this.administradorCombate = pAdministradorCombate;
+            this.hiloMutante = new ArrayList<>();
+        }
+
+    public void iniciarJuego(int pCantidad){
+        generarEquipos(pCantidad);
+        iniciarMovimiento();
+        controlarBatalla();
+        finalizarJuego();
+    }  
+    public void generarEquipos(int pCantidad){
+        this.campoBatalla.crearEquipos(pCantidad);
+    }
+    public void iniciarMovimiento(){
+        this.hiloMutante.clear();
+        for (Mutante mutante: this.campoBatalla.getEquipo1().getMutantes()){
+            HiloMutante hilo = new HiloMutante(mutante, 
+                this.campoBatalla, this.administradorCombate);
+            this.hiloMutante.add(hilo);
+        }
+        for (Mutante mutante: this.campoBatalla.getEquipo2().getMutantes()){
+            HiloMutante hilo = new HiloMutante(mutante, 
+                this.campoBatalla, this.administradorCombate);
+            this.hiloMutante.add(hilo);
+        }
+        for (HiloMutante hilo: this.hiloMutante){
+            hilo.start();
+        }
+    }
+    public void controlarBatalla(){
+        for(HiloMutante hilo: this.hiloMutante){
+            try{hilo.join();
+            }catch (InterruptedException e){
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+    public void finalizarJuego(){
+        for (HiloMutante hilo: this.hiloMutante){
+            hilo.detener();
+        }
+        Equipo ganador= this.campoBatalla.obtenerGanador();
+        if (ganador != null){
+            System.out.println("La batalla ha terminado");
+            System.out.println("El equipo ganador es: "+ ganador); //falta para que muestre el equipo
+        }
+    }
+    public void nuevaBatalla(){
+        this.hiloMutante.clear();
+        this.campoBatalla= new CampoBatalla();
+    }
+}
